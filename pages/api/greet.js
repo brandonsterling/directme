e55f60becdb9feb05ea551d79e55570e405a6dc0
brandonsterling <brandonsterling@knights.ucf.edu>
2023-01-19 14:49:58 -0500

    Removing Slack bolt

--- pages/api/greet.js
+++ pages/api/greet.js
@@ -1,22 +1,41 @@
-import { App } from "@slack/bolt";
-
-export default async function handler(request, response) {
+export default async function handler(req, res) {
   const signingSecret = process.env.SLACK_SIGNING_SECRET;
   const botToken = process.env.BOT_TOKEN;
 
-  const app = new App({
-    signingSecret: signingSecret,
-    token: botToken,
-  });
-  async () => {
-    await app.start(3000 || 12000);
+  if (req.method === "POST") {
+    const { team_id, response_url, user_id } = req.body;
 
-    app.message("quote", async ({ message, say }) => {
-      let resp = await fetch(`https://api.quotable.io/random`);
-      const quote = resp.data.content;
-      await say(`Hello, <@${message.user}>, ${quote}`);
-    });
+    try {
+      const headers = {
+        Authorization: `Bearer ${botToken}`,
+        "Content-type": "application/json",
+      };
 
-    console.log(`⚡️ Bolt app is running!`);
-  };
+      let raw = {
+        response_type: "in_channel",
+        blocks: [
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "I'm a brand new Slack bot!",
+            },
+          },
+        ],
+      };
+
+      const requestOptions = {
+        method: "POST",
+        headers,
+        body: JSON.stringify(raw),
+      };
+
+      await fetch(`${response_url}`, requestOptions);
+      res.status(200).end();
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(404).end();
+  }
 }
